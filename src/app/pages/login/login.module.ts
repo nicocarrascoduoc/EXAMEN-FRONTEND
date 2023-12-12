1b7169adef0f370febd2ee3f950d8570e83835b5
nicocarrascoduoc <nico.carrascof@duocuc.cl>
2023-12-12 17:03:45 -0300

    T
    Y

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    SharedModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
